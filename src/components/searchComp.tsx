a335d35e4468448d5425b76bc3882c2250bfdc03
leahSalemon <leah212122@gmail.com>
2026-02-14 20:56:08 +0200

    display 'no result' only after first search

--- src/components/searchComp.tsx
+++ src/components/searchComp.tsx
@@ -8,12 +8,14 @@ import './searchComp.css';
 const SearchComp = () => {
   const [artists, setArtists] = useState<Artist[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [wasSearched, setWasSearched] = useState<boolean>(false);
 
   const handleSearch = async (term: string) => {
     setLoading(true);
     try {
       const data = await fetchArtistsByName(term);
       setArtists(data || []);
+      setWasSearched(true);
     }
     catch (error) {
       console.error("Error fetching artists:", error);
@@ -22,6 +24,12 @@ const SearchComp = () => {
       setLoading(false);
     }
   };
+
+  const displayNoResults = () => {
+    if (wasSearched &&  !loading) {
+      return <p>No results found yet.</p>;
+    }
+  }
 
   const displayArtists = () => {
     return artists.length > 0 ? (
@@ -34,13 +42,10 @@ const SearchComp = () => {
   return (
     <div className="search-comp">
       <h1>Music Search</h1>
-    
       <SearchInput onSearch={handleSearch} loadingSearch={loading} />
 
-      {loading && <p>Searching...</p>}
-
       <div className="artist-results">
-        {displayArtists()}
+        { artists.length > 0 ? displayArtists() : displayNoResults() }
       </div>
     </div>
   );
